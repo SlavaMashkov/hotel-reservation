de7c912d4074c65a01b580f46618e7f6bb0a5c4f
Slava Mashkov <mashkov.vd@gmail.com>
2024-06-16 21:12:58 +0300

    Connect to mongo

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/SlavaMashkov/hotel-reservation/api"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log/slog"
 )
+
+const dbuser = "root"
+const dbpassword = "example"
+const dburi = "mongodb://localhost:27017"
 
 func main() {
 	listenPort := flag.String("listenPort", ":5000", "The listen port of the API server")
 	flag.Parse()
+
+	credentials := options.Credential{
+		Username: dbuser,
+		Password: dbpassword,
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi).SetAuth(credentials))
+	if err != nil {
+		slog.Error("client creation", slog.String("error", err.Error()))
+		return
+	}
+	defer client.Disconnect(context.TODO())
 
 	app := fiber.New()
 
