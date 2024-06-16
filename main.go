39b581b3fff2bb5d6f7d2905acdb5f006c11f0b6
Slava Mashkov <mashkov.vd@gmail.com>
2024-06-16 17:35:17 +0300

    Add test handler

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log/slog"
+import (
+	"github.com/gofiber/fiber/v2"
+	"log/slog"
+)
 
 func main() {
-	slog.Info("Hello, World!")
+	app := fiber.New()
+	slog.Info("start server", slog.Int("port", 5000))
+	app.Get("/ping", handlePing())
+	app.Listen(":5000")
+}
+
+func handlePing() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		return c.JSON(map[string]string{"message": "pong"})
+	}
 }
