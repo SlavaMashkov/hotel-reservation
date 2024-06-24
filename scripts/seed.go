c0aac56fef34ca91595f3f04043e1fa329c49ad5
Slava Mashkov <mashkov.vd@gmail.com>
2024-06-24 22:09:10 +0300

    Split seed script into separate functions and make drop db on each seed

--- scripts/seed.go
+++ scripts/seed.go
@@ -10,26 +10,18 @@ import (
 	"log/slog"
 )
 
+var (
+	client     *mongo.Client
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+	ctx        = context.Background()
+)
+
 func main() {
 	slog.Info("seeding the database")
 
-	credentials := options.Credential{
-		Username: db.USERNAME,
-		Password: db.PASSWORD,
-	}
-
-	ctx := context.Background()
-
-	// TODO: extract connection initialization logic
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.URI).SetAuth(credentials))
-	if err != nil {
-		slog.Error("client creation", slog.String("error", err.Error()))
-		return
-	}
+	initVars()
 	defer client.Disconnect(context.TODO())
-
-	hotelStore := db.NewHotelStoreMongo(client)
-	roomStore := db.NewRoomStoreMongo(client)
 
 	hotel := types.Hotel{
 		Name:     "Hilton",
@@ -37,13 +29,10 @@ func main() {
 		Rooms:    make([]primitive.ObjectID, 0),
 	}
 
-	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+	insertedHotel, err := seedHotel(&hotel)
 	if err != nil {
-		slog.Error("hotel insertion", slog.String("error", err.Error()))
 		return
 	}
-
-	slog.Info("hotel inserted", slog.String("id", insertedHotel.ID.Hex()))
 
 	rooms := []types.Room{
 		{
@@ -56,15 +45,60 @@ func main() {
 		},
 	}
 
+	err = seedRooms(rooms, insertedHotel)
+	if err != nil {
+		return
+	}
+
+	slog.Info("database seeded")
+}
+
+func initVars() {
+	credentials := options.Credential{
+		Username: db.USERNAME,
+		Password: db.PASSWORD,
+	}
+
+	ctx = context.Background()
+	var err error
+
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(db.URI).SetAuth(credentials))
+	if err != nil {
+		slog.Error("client creation", slog.String("error", err.Error()))
+		return
+	}
+
+	if err = client.Database(db.NAME).Drop(ctx); err != nil {
+		slog.Error("database dropping", slog.String("error", err.Error()))
+		return
+	}
+
+	hotelStore = db.NewHotelStoreMongo(client)
+	roomStore = db.NewRoomStoreMongo(client)
+
+}
+
+func seedHotel(hotel *types.Hotel) (*types.Hotel, error) {
+	insertedHotel, err := hotelStore.InsertHotel(ctx, hotel)
+	if err != nil {
+		slog.Error("hotel insertion", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	slog.Info("hotel inserted", slog.String("id", insertedHotel.ID.Hex()))
+	return insertedHotel, nil
+}
+
+func seedRooms(rooms []types.Room, hotel *types.Hotel) error {
 	for _, room := range rooms {
-		_, err = roomStore.InsertRoom(ctx, insertedHotel.ID.Hex(), &room)
+		_, err := roomStore.InsertRoom(ctx, hotel.ID.Hex(), &room)
 		if err != nil {
 			slog.Error("room insertion", slog.String("error", err.Error()))
-			return
+			return err
 		}
 
 		slog.Info("room inserted", slog.String("id", room.ID.Hex()))
 	}
 
-	slog.Info("database seeded")
+	return nil
 }
