65481deadac218bbe9b5f16c13157e272d1dbc8d
Slava Mashkov <mashkov.vd@gmail.com>
2024-06-24 23:33:32 +0300

    Parse query params from url, add example requests

--- api/hotel_handler.go
+++ api/hotel_handler.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"github.com/SlavaMashkov/hotel-reservation/db"
+	"github.com/SlavaMashkov/hotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
+	"log/slog"
 )
 
 type HotelHandler struct {
@@ -18,9 +20,17 @@ func NewHotelHandler(hotelStore db.HotelStore, roomStore db.RoomStore) *HotelHan
 }
 
 func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
+	var queryParams types.QueryHotelParams
+	if err := c.QueryParser(&queryParams); err != nil {
+		return err
+	}
+
+	slog.Info("query params", slog.Any("value", queryParams))
+
 	hotels, err := h.hotelStore.GetHotels(c.Context())
 	if err != nil {
 		return err
 	}
+
 	return c.JSON(hotels)
 }
